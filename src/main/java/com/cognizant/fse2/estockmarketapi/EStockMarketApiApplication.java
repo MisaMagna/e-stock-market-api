eefb0b012f5a825926306125279a04d7b70e2d1f
Misael Pereira Diaz <Misael.PereiraDiaz@cognizant.com>
2022-06-04 11:13:38 +0200

    Init Project

---
+++ src/main/java/com/cognizant/fse2/estockmarketapi/EStockMarketApiApplication.java
@@ -0,0 +1,11 @@
+package com.cognizant.fse2.estockmarketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EStockMarketApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EStockMarketApiApplication.class, args);
+	}
+}
