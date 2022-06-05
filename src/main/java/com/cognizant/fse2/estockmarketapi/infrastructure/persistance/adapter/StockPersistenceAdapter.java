dfec88ce76888867b9b65202691d66ed3cc04ef6
Misael Pereira Diaz <Misael.PereiraDiaz@cognizant.com>
2022-06-05 19:47:51 +0200

    Added stock persistence logic

--- src/main/java/com/cognizant/fse2/estockmarketapi/infrastructure/persistance/adapter/StockPersistenceAdapter.java
+++ src/main/java/com/cognizant/fse2/estockmarketapi/infrastructure/persistance/adapter/StockPersistenceAdapter.java
@@ -1,29 +1,47 @@
 package com.cognizant.fse2.estockmarketapi.infrastructure.persistance.adapter;
 
+import com.cognizant.fse2.estockmarketapi.domain.exception.CompanyNotFoundException;
 import com.cognizant.fse2.estockmarketapi.domain.model.Stock;
 import com.cognizant.fse2.estockmarketapi.domain.port.StockPersistencePort;
-import com.cognizant.fse2.estockmarketapi.infrastructure.persistance.repository.StockRepository;
+import com.cognizant.fse2.estockmarketapi.infrastructure.persistance.document.CompanyDocument;
+import com.cognizant.fse2.estockmarketapi.infrastructure.persistance.repository.CompanyRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class StockPersistenceAdapter implements StockPersistencePort {
 
-    private final StockRepository repository;
+    private final CompanyRepository repository;
 
-    public StockPersistenceAdapter(StockRepository repository) {
+    public StockPersistenceAdapter(CompanyRepository repository) {
         this.repository = repository;
     }
 
     @Override
     public List<Stock> findAllByCompanyCodeAndDateRange(String companyCode, LocalDate startDate, LocalDate endDate) {
-        return null;
+        Optional<CompanyDocument> document = repository.findById(companyCode);
+        if (document.isEmpty()) {
+            String message = String.format("Company with id %s does not exists", companyCode);
+            throw new CompanyNotFoundException(message);
+        }
+        // TODO: REFACTOR
+        return document.get().getStocks().stream()
+                .filter(stock -> (stock.getDate().isEqual(startDate) || stock.getDate().isAfter(startDate)) && (stock.getDate().isEqual(endDate) || stock.getDate().isBefore(endDate)))
+                .collect(Collectors.toList());
     }
 
     @Override
-    public Stock save(String companyCode, Stock stock) {
-        return null;
+    public void save(String companyCode, Stock stock) {
+        Optional<CompanyDocument> document = repository.findById(companyCode);
+        if (document.isEmpty()) {
+            String message = String.format("Company with id %s does not exists", companyCode);
+            throw new CompanyNotFoundException(message);
+        }
+        document.get().getStocks().add(stock);
+        repository.save(document.get());
     }
 }
