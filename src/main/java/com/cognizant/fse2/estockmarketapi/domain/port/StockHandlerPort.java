415da6d75bfe5adeac05d6059da3af19e1e43385
Misael Pereira Diaz <Misael.PereiraDiaz@cognizant.com>
2022-06-06 19:35:06 +0200

    Move validation to handle port

--- src/main/java/com/cognizant/fse2/estockmarketapi/domain/port/StockHandlerPort.java
+++ src/main/java/com/cognizant/fse2/estockmarketapi/domain/port/StockHandlerPort.java
@@ -1,12 +1,15 @@
 package com.cognizant.fse2.estockmarketapi.domain.port;
 
 import com.cognizant.fse2.estockmarketapi.domain.model.Stock;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.List;
 
+@Validated
 public interface StockHandlerPort {
     List<Stock> get(String companyCode, LocalDate startDate, LocalDate endDate);
 
-    void add(String companyCode, Stock stock);
+    void add(String companyCode, @Valid Stock stock);
 }
